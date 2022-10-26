09f8833efd972c8c811724f988ab784d735a8735
andre_gthv1mg <anrei.kinder.170@gmail.com>
2022-10-26 10:43:53 +0500

    list of components

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-Vue.component('task',{
-	template : '<li><slot></slot></li>',
+Vue.component('task-list',{
+	template : `
+					<div>
+						<task v-for="task in tasks"> {{task.task}} </task>
+					</div>`,
 	data(){
-
-		return {
-			message : 'foobar'
-		}
+		return  {
+			tasks : [
+				{task: 'Go to the work', complete : true},
+				{task: 'Go to the home', complete : false},
+				{task: 'Go to the coffee', complete : true},
+				{task: 'Go to the farm', complete : false}
+				]
+			}
+		
 	}
 })
 
+Vue.component('task',{
+	template : '<li><slot></slot></li>',
+})
+
+
+ 
 var app = new Vue({
 	el: "#root",
 })
