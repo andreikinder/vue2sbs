fd111b7c0fa9e138179ecf7d74f64fe63bc8a360
andre_gthv1mg <anrei.kinder.170@gmail.com>
2022-11-01 08:46:15 +0500

    named slots of modal -header - footer -body

--- main.js
+++ main.js
@@ -1,42 +1,31 @@
-window.Event = new class {
-
-	constructor(){
-		this.vue = new Vue()
-	}
-
-	fire(event, data = null) {
-		this.vue.$emit(event, data)
-	}
-
-	listen(event, callback){
-		this.vue.$on(event, callback)
-	}
-}
-
-Vue.component('coupon',{
-	template : `<input placeholder="Enter your coupon" @blur="onCouponApplied">`,
-	methods : {
-		onCouponApplied() {
-			Event.fire('applied')
-		}
-	}
+Vue.component('modal',{
+	template : `
+	<div class="modal is-active">
+		<div class="modal-background"></div>
+		<div class="modal-card">
+		<header class="modal-card-head">
+			<p class="modal-card-title">
+			<slot name="header"></slot>
+			</p>
+			<button class="delete" aria-label="close"></button>
+		</header>
+		<section class="modal-card-body">
+			<slot>Default text</slot>
+		</section>
+		<footer class="modal-card-foot">
+			<slot name="footer">
+				<button class="button is-success">Okey</button>
+			</slot>
+		
+		</footer>
+		</div>
+ 	</div>`
 })
 
  
 
  
 var app = new Vue({
-	el: "#root",
-	data : {
-		couponApplied : false
-	},
-	methods : {
-		onCouponApplied() {
-			this.couponApplied = true
-		}
-	},
-	created() {
-		Event.listen('applied', () => alert('Handling it !'))
-	}
+	el: "#root"
 })
  
