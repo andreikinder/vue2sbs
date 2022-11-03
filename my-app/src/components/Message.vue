a00a9198088c78f5b11430b707cea16f47d1a4d4
andre_gthv1mg <anrei.kinder.170@gmail.com>
2022-11-03 10:09:14 +0500

    init vue project

---
+++ my-app/src/components/Message.vue
@@ -0,0 +1,28 @@
+<template>
+
+	<div class="box">
+		<p>
+			<slot></slot>
+		</p>
+	</div>
+
+</template
+
+
+<script>
+	export default {
+
+
+	}
+
+</script>
+
+
+<style>
+	.box{
+		paddint: 50px;
+		background: #ccc;
+		border: 1px solid #4d4d4d;
+	}
+
+</style>
