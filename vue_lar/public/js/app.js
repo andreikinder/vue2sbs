7d155a2cdd1d102edcbe85b490953ac182750612
andre_gthv1mg <anrei.kinder.170@gmail.com>
2022-11-11 09:26:29 +0500

    form3

--- vue_lar/public/js/app.js
+++ vue_lar/public/js/app.js
@@ -48,34 +48,60 @@ class Form {
 		for (let field in this.OriginalData) {
 			this[field] = ''
 		}
+		this.errors.clear()
 	}
 
 	data() {
-		let data = Object.assign({}, this);
-		delete data.OriginalData
-		delete data.errors
+
+		let data = {}
+
+		for (let property in this.OriginalData) {
+			data[property] = this[property]
+		}
+		// let data = Object.assign({}, this);
+		// delete data.OriginalData
+		// delete data.errors
 
 		return data
 	}
 
 	submit(requestType, url) {
-		axios[requestType](url, this.data() )
-		.then(this.onSuccess.bind(this))
-		.catch(this.onFail.bind(this))
+		return new Promise( (resolve, reject) => {
+			axios[requestType](url, this.data() )
+			.then( response => { 
+				this.onSuccess(response.data)
+				resolve(response.data)
+			 } )
+			 .catch(error => {
+				 this.onFail(error.response.data.errors)
+
+				 reject(error.response.data.errors)
+			 })
+			
+		})
+
 	}
 
-	onSuccess(response) {
+	post(url) {
+		return this.submit('post', url)
+	}
+
+	delete(url) {
+		return this.submit('delete', url)
+	}
+
+	onSuccess(data) {
 		///response => this.onSuccess(response)
 
-		alert(response.data.message)
-		this.errors.clear()
+		alert(data.message)
+
 		// this.name=''
 		// this.description=''
 		 this.reset()
 	}
-	onFail(error) {
+	onFail(errors) {
 		//this.errors.record( error.response.data.errors)
-		this.errors.record( error.response.data.errors)
+		this.errors.record(errors)
 	}
 }
 
@@ -91,7 +117,9 @@ new Vue({
 
 	methods : {
 		onSubmit() {
-			this.form.submit('post', '/projects')
+			this.form.post('/projects')
+			.then(data => console.log(data))
+			.catch(errors => console.log(errors))
 		},
 	}
 })
